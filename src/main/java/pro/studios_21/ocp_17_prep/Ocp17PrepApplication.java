85e8488ec65abcb9953b3dd583b5a969f5f300d3
ChandanT21 <vichandan@me.com>
2023-02-01 12:15:44 +0530

    Initial Commit

---
+++ src/main/java/pro/studios_21/ocp_17_prep/Ocp17PrepApplication.java
@@ -0,0 +1,13 @@
+package pro.studios_21.ocp_17_prep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ocp17PrepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ocp17PrepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/studios_21/ocp_17_prep/Ocp17PrepApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.studios_21.ocp_17_prep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ocp17PrepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
