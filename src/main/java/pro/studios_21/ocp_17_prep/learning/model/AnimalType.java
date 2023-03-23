afc09dfec55bd2bb45feac7d355751331edd625b
ChandanT21 <vichandan@me.com>
2023-03-23 13:34:39 -0400

    CleanUp application

--- src/main/java/pro/studios_21/ocp_17_prep/learning/model/AnimalType.java
+++
@@ -1,5 +0,0 @@
-package pro.studios_21.ocp_17_prep.learning.model;
-
-public enum AnimalType {
-
-}
--- src/main/java/pro/studios_21/ocp_17_prep/learning/model/Employee.java
+++
@@ -1,7 +0,0 @@
-package pro.studios_21.ocp_17_prep.learning.model;
-
-import lombok.Data;
-
-@Data
-public class Employee {
-}
